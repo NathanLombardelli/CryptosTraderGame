28dea1e9b900f00f4221e72f90deb9f0535a5614
Nathan Lombardelli <55028792+NathanLombardelli@users.noreply.github.com>
2023-07-12 16:17:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# template_React
+template_React
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+import {Title} from "./assets/Title.jsx";
+
+function App() {
+
+  return (
+    <>
+        <Title text={"Hello World"}/>
+    </>
+  )
+}
+
+export default App
